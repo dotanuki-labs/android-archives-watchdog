cf350a561fa20ec5940de7fa7cc356c557f68de6
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-09 22:09:05 +0200

    Improves CLI help menu

--- src/main/kotlin/io/dotanuki/arw/cli/ARWCommand.kt
+++ src/main/kotlin/io/dotanuki/arw/cli/ARWCommand.kt
@@ -5,18 +5,20 @@ import com.github.ajalt.clikt.core.context
 
 class ARWCommand : CliktCommand(
     name = "arw <command> <options>",
-    help = "A watchdog for your Android releasable artifacts",
     printHelpOnEmptyArgs = true,
     epilog =
     """
+    Available commands:
+    
+    • analyse : dumps basic information about the target APK or AAB${"\u0085"}
+    • generate : extracts info about an APK or AAB into a baseline${"\u0085"}
+    • compare : compares the target APK or AAB with a baseline
+    
     Examples:
     
-    ```
-    $> arw version
-    $> arw analyse
-    $> arw generate <option>
-    $> arw compare <options>
-    ```
+    $> arw analyse --path=/path/to/my.aab${"\u0085"}
+    $> arw generate --path=/path/to/my.apk --mappings=/path/to/mappings.txt${"\u0085"}
+    $> arw compare -p=/path/to/my.apk -b=./arw.toml --o=json
     """.trimIndent()
 ) {
 
