792087de32611c0127994a0f3f3c06aa3001f40f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-12 12:49:21 +0200

    Packages executable fatjar as an artifact

---
+++ scripts/package.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${dir%/*}"
+
+readonly version=$(grep "latest" src/main/resources/versions.properties | sed "s/latest=//g")
+readonly fatjar="build/libs/arw-$version.jar"
+readonly output_folder="build/bin"
+readonly target_binary="$output_folder/arw"
+
+if ! test -f "$fatjar"; then
+  echo "✗ Error : missing target fatjar!"
+  echo
+  exit 1
+fi
+
+rm -rf "$"$output_folder""
+mkdir -p "$output_folder"
+touch "$output_folder/arw"
+echo "#! /usr/bin/env bash" >>"$target_binary"
+echo "" >>"$target_binary"
+echo "exec java -Xmx512m -jar \$0 \"\$@\"" >>"$target_binary"
+echo "" >>"$target_binary"
+echo "" >>"$target_binary"
+echo "" >>"$target_binary"
+
+cat "$fatjar" >>"$target_binary"
+chmod +x "$target_binary"
