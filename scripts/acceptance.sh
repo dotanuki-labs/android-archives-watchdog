27b6b8084958f916eaecac14a833bbce07b87c4f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-13 20:13:53 +0200

    Implements CLI for releasable overview

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -8,7 +8,17 @@ cd "${dir%/*}"
 readonly arw="build/bin/arw"
 
 test_no_arguments_passed() {
-  "$arw" | grep "Usage: arw <command> <options>" >/dev/null
+  "$arw" | grep "Usage" >/dev/null
+}
+
+test_overview_console() {
+  "$arw" overview
+}
+
+test_overview_values() {
+  overview=$("$arw" overview --json)
+  (( $(echo "$overview" | jq '.min_sdk') == 28 ))
+  (( $(echo "$overview" | jq '.target_sdk') == 33 ))
 }
 
 echo
@@ -19,6 +29,8 @@ echo "→ Running tests"
 echo
 
 test_no_arguments_passed
+test_overview_console
+test_overview_values
 
 echo "🔥 Success"
 echo
---
+++ src/main/kotlin/io/dotanuki/arw/utils/TerminalExtensions.kt
@@ -0,0 +1,5 @@
+package io.dotanuki.arw.utils
+
+import com.github.ajalt.mordant.terminal.Terminal
+
+fun Terminal.emptyLine() = this.println()
