00eff0db48de8444867f535551ea043988f04695
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-10-16 01:06:18 +0200

    Implements overview support for release artefacts

--- src/test/kotlin/io/dotanuki/arw/shared/analyser/AndroidArtifactAnalyserTests.kt
+++ src/test/kotlin/io/dotanuki/arw/shared/analyser/AndroidArtifactAnalyserTests.kt
@@ -1,33 +1,42 @@
 package io.dotanuki.arw.shared.analyser
 
-import arrow.core.raise.recover
 import com.google.common.truth.Truth.assertThat
 import io.dotanuki.arw.helpers.fixtureFromResources
+import io.dotanuki.arw.helpers.raiseAware
 import io.dotanuki.arw.overview.ReleasableOverview
 import org.junit.Test
 
 class AndroidArtifactAnalyserTests {
 
-    @Test fun `should analyse a debug apk with success`() {
-        recover(
-            block = {
-                val target = fixtureFromResources("app-debug.apk")
-                val overview = AndroidArtifactAnalyser.overview(target)
+    @Test fun `should analyse a debug apk with success`() = raiseAware {
+        val target = fixtureFromResources("app-debug.apk")
+        val overview = AndroidArtifactAnalyser.overview(target)
 
-                val expected = ReleasableOverview(
-                    applicationId = "io.dotanuki.norris.android.debug",
-                    debuggable = true,
-                    minSdk = 28,
-                    targetSdk = 33,
-                    totalPermissions = 5,
-                    dangerousPermissions = true
-                )
-
-                assertThat(overview).isEqualTo(expected)
-            },
-            recover = {
-                throw AssertionError("Should not recover on this test! -> Error = $it")
-            }
+        val expected = ReleasableOverview(
+            applicationId = "io.dotanuki.norris.android.debug",
+            debuggable = true,
+            minSdk = 28,
+            targetSdk = 33,
+            totalPermissions = 5,
+            dangerousPermissions = true
         )
+
+        assertThat(overview).isEqualTo(expected)
+    }
+
+    @Test fun `should analyse a release apk with success`() = raiseAware {
+        val target = fixtureFromResources("app-release.apk")
+        val overview = AndroidArtifactAnalyser.overview(target)
+
+        val expected = ReleasableOverview(
+            applicationId = "io.dotanuki.norris.android",
+            debuggable = false,
+            minSdk = 28,
+            targetSdk = 33,
+            totalPermissions = 2,
+            dangerousPermissions = false
+        )
+
+        assertThat(overview).isEqualTo(expected)
     }
 }
