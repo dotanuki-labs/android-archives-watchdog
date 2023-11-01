a3f811b7429f362035bb99bd5a272b043b7f1ec3
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-11-01 16:34:45 +0100

    Adds support to Gradle build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,14 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.enterprise") version "3.15.1"
 }
 
 rootProject.name = "android-archives-watchdog"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
