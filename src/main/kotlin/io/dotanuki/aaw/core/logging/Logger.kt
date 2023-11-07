c16fbf6eb24c75b8c575a799dadeb82277d04b85
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2023-11-07 20:08:32 +0100

    Moves ErrorReporter to Logging context

--- src/main/kotlin/io/dotanuki/aaw/core/logging/Logger.kt
+++ src/main/kotlin/io/dotanuki/aaw/core/logging/Logger.kt
@@ -9,13 +9,18 @@ import com.github.ajalt.mordant.rendering.TextColors.cyan
 import com.github.ajalt.mordant.rendering.TextColors.red
 import com.github.ajalt.mordant.terminal.Terminal
 
-data class Logger(val terminal: Terminal) {
+data class Logger(
+    private val terminal: Terminal,
+    private val verboseMode: Boolean
+) {
     fun newLine() {
         terminal.println()
     }
 
-    fun log(message: String) {
-        terminal.println(message)
+    fun debug(message: String) {
+        if (verboseMode) {
+            terminal.println(message)
+        }
     }
 
     fun info(message: String) {
