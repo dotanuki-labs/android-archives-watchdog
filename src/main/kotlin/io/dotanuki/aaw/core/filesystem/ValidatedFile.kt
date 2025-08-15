393dbdf8080a35d0c1f90dce5092337360978c61
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-08-15 22:20:23 +0200

    refactor: migrate from raise dsl (#523)

--- src/main/kotlin/io/dotanuki/aaw/core/filesystem/ValidatedFile.kt
+++ src/main/kotlin/io/dotanuki/aaw/core/filesystem/ValidatedFile.kt
@@ -5,23 +5,24 @@
 
 package io.dotanuki.aaw.core.filesystem
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import io.dotanuki.aaw.core.errors.AawError
-import io.dotanuki.aaw.core.errors.ErrorAware
 import java.io.File
 
+@Suppress("ReturnCount")
 object ValidatedFile {
-    context (ErrorAware)
-    operator fun invoke(filePath: String): String =
-        File(filePath)
-            .run {
-                if (!exists()) {
-                    raise(AawError("$filePath does not exist"))
-                }
+    operator fun invoke(filePath: String): Either<AawError, String> {
+        val target = File(filePath)
+        if (!target.exists()) {
+            return AawError("$filePath does not exist").left()
+        }
 
-                if (!isFile()) {
-                    raise(AawError("$filePath is not a file"))
-                }
-            }.let {
-                filePath
-            }
+        if (!target.isFile()) {
+            return AawError("$filePath is not a file").left()
+        }
+
+        return filePath.right()
+    }
 }
